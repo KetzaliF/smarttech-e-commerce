e1a1ea107e0dd32e1599163a63a58d663c99c305
KetzaliF <ketzalihoffman@gmail.com>
2021-12-04 13:47:07 -0600

    recovery responsive

--- src/pages/RecoveryPassword.jsx
+++ src/pages/RecoveryPassword.jsx
@@ -7,13 +7,12 @@ const RecoveryPassword = () => {
 	return (
 		<div className="PasswordRecovery">
 			<div className="PasswordRecovery-container">
-				<img src={Logo} alt="logo" className="recovery-logo" />
-				<h1 className="title">Password recovery</h1>
-				<p className="subtitle">Inform the email address used to create your account</p>
+				<h1 className="title-recovery">Password recovery</h1>
+				<p className="subtitle-recovery">Inform the email address used to create your account</p>
 				<form action="/" className="form">
 					<label htmlFor="email" className="label">Email address</label>
 					<input type="text" id="email" className="input input-email" />
-					<input type="submit" value="Confirm" className="primary-button login-button" />
+					<input type="submit" value="Confirm" className="btn-primary login-btn" />
 				</form>
 			</div>
 		</div>
